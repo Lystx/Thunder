ed9bed2862a0627a6207d4e9d6a3c1b6cafed453
Lystx <63949927+Lystx@users.noreply.github.com>
2021-03-10 20:17:18 +0100

    Delete Vera directory

--- Vera/src/main/java/io/vera/meta/entity/living/monster/FlyingEntityMeta.java
+++
@@ -1,9 +0,0 @@
-
-package io.vera.meta.entity.living.monster;
-
-import io.vera.meta.entity.living.InsentientEntityMeta;
-
-
-
-public interface FlyingEntityMeta extends InsentientEntityMeta {
-}
--- Vera/src/main/java/io/vera/world/gen/FeatureGenerator.java
+++
@@ -1,9 +0,0 @@
-
-package io.vera.world.gen;
-
-import io.vera.world.opt.GenOpts;
-
-public interface FeatureGenerator {
-
-    void generate(int chunkX, int chunkZ, GeneratorContext context);
-}
