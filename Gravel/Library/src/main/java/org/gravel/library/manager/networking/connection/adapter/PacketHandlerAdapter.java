a585d4442aba522c2bf833fe37aa227555de2673
Lystx <63949927+Lystx@users.noreply.github.com>
2021-04-10 17:13:43 +0200

    Delete Gravel directory

--- Gravel/Library/src/main/java/org/gravel/library/manager/networking/connection/adapter/PacketHandlerAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.gravel.library.manager.networking.connection.adapter;
-
-
-import org.gravel.library.manager.networking.connection.packet.Packet;
-
-public abstract class PacketHandlerAdapter {
-
-    public abstract void handle(Packet packet);
-}
--- Gravel/Library/src/main/java/org/gravel/library/manager/user/UserStatus.java
+++
@@ -1,9 +0,0 @@
-package org.gravel.library.manager.user;
-
-public enum UserStatus {
-
-    ONLINE,
-    AFK,
-    DO_NOT_DISTURB,
-    OFFLINE;
-}
