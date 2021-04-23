9a30a908c625449026f9e384eac2a73f2ee8f8f9
Lystx <63949927+Lystx@users.noreply.github.com>
2021-04-23 10:12:08 +0200

    Delete Gravel directory

--- Gravel/Library/src/main/java/org/gravel/library/manager/networking/connection/adapter/PacketHandlerAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.gravel.library.manager.networking.connection.adapter;
-
-
-import io.thunder.manager.packet.ThunderPacket;
-
-public abstract class PacketHandlerAdapter {
-
-    public abstract void handle(ThunderPacket packet);
-}
